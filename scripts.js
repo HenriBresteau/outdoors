72eccfc8541e217e4326bcb4b7e365791cf263f2
henriB <henri.bretseau@gmail.com>
2021-03-31 17:51:03 +0200

    open / close Menu

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 //start
+
+const openMenu = document.querySelector(".open");
+const navList = document.querySelector(".nav-list");
+const closeMenu = document.querySelector(".close");
+openMenu.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
+closeMenu.addEventListener("click", () => {
+  navList.classList.remove("active");
+});
